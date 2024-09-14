a351e230f0e5d11fe4cb4fb7a0cfdd9e95835dd1
Francesba123 <165425907+Francesba123@users.noreply.github.com>
2024-09-14 15:43:53 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Código JavaScript para interatividade pode ser adicionado aqui
+// Adicione funcionalidades JavaScript aqui se necessário
+document.addEventListener('DOMContentLoaded', () => {
+    // Código para adicionar interações ou efeitos dinâmicos
 });
